ef7b56f9978e7d875bf90375adad01d62883490c
Bigwillie001 <davidtestimony183@gmail.com>
2026-03-16 23:21:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function copyCA() {
+function copyCA(){
 
 const contract = document.getElementById("contract").innerText;
 
@@ -8,8 +8,38 @@ const toast = document.getElementById("copy-toast");
 
 toast.classList.add("show");
 
-setTimeout(() => {
+setTimeout(()=>{
 toast.classList.remove("show");
-}, 2000);
+},2000);
 
-           }
+}
+
+
+/* LIVE PRICE FETCH */
+
+async function fetchPrice(){
+
+try{
+
+const res = await fetch(
+"https://api.dexscreener.com/latest/dex/tokens/atVjZ7uM8sVrLFi5Xe1JiLGW6mW9pvQdTCWzhNFpump"
+);
+
+const data = await res.json();
+
+const price = data.pairs[0].priceUsd;
+
+document.getElementById("price").innerText =
+"$" + Number(price).toFixed(6);
+
+}catch(e){
+console.log("Price fetch error",e);
+}
+
+}
+
+fetchPrice();
+
+/* auto update */
+
+setInterval(fetchPrice,10000);
